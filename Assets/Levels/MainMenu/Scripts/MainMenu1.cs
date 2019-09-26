1db8b9619b4c29ead666b6753e7ce6339c5203e3
Anonimul007 <brocho1337@gmail.com>
2019-09-26 22:46:12 +0300

    Update-Discord Rich Presence and Main Menu

---
+++ Assets/Levels/MainMenu/Scripts/MainMenu1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu1 : MonoBehaviour
+{
+    public void Play () {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Debug.Log("");
+    }
+
+    public void Discord () {    
+        Application.OpenURL("https://discord.gg/Jm5XAbF");
+    }
+
+    public void SetFullscreen (bool isFullscreen) { 
+        Screen.fullScreen = isFullscreen;
+}
+
+    public void Options () {    
+        SceneManager.LoadScene(0);
+    }
+}
